94980ebd455acc0d99e384bb116bd67def1ea45b
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2021-07-08 14:04:16 +0100

    Fix an issue where a UID could match ZFS bytes

--- ZFS-Quota-Client.sh
+++ ZFS-Quota-Client.sh
@@ -4,9 +4,7 @@
 # IPPP, Durham University
 
 servers[0]="/mt/home"
-servers[1]="/mt/fielding-home"
-servers[2]="/mt/user-batch"
-servers[3]="/mt/group-batch"
+servers[1]="/mt/storage"
 
 if [ -z "$1" ]; then
   QUSER=$USER
@@ -38,14 +36,19 @@ printf "\n Quota Report for $QUSER\n"
 printf ' %-30s %-15s %-15s %-25s\n' "Mount Point" "Used" "Total" "Last Checked";
 QUID=$(id -u $QUSER)
 for i in "${servers[@]}"; do
-  zquota=$(cat $i/quota.zfs 2>/dev/null | grep -m 1 -w $QUID);
+  zquota=$(cat $i/quota.zfs 2>/dev/null | grep $QUID);
   if [[ ! -z "$zquota" ]]; then
-    zused=$(echo $zquota | awk -F'::' '{print $2}' | numfmt --to=iec);
-    ztotal=$(echo $zquota | awk -F'::' '{print $3}');
-    if [[ $ztotal -ne "none" ]]; then 
-      ztotal=$(echo $ztotal | numfmt --to=iec);
-    fi
-    zperc=$(echo $zquota | awk -F'::' '{print $4}');
+    for ii in $zquota; do 
+      zuid=$(echo $ii | awk -F'::' '{print $1}')
+      if  [[ $zuid == $QUID ]]; then
+        zused=$(echo $ii | awk -F'::' '{print $2}' | numfmt --to=iec);
+        ztotal=$(echo $ii | awk -F'::' '{print $3}');
+        if [[ $ztotal -ne "none" ]]; then
+          ztotal=$(echo $ztotal | numfmt --to=iec);
+        fi
+        zperc=$(echo $ii | awk -F'::' '{print $4}');
+      fi
+    done 
     zage=$(date +"%c" -d @$(stat -c %Z $i/quota.zfs))
     printf ' %-30s %-15s %-15s %-25s\n' "$i" "$zused ($zperc)" "$ztotal" "$zage";
   fi
